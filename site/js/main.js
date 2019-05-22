51dd3a797aab7ff87af744965e196b8b0d8b207b
itggot-malek-amer <malek.amer@elev.ga.ntig.se>
2019-05-22 09:51:55 +0200

    början av sidan
    jag har byggt grunden för sidan och håller på med de sista funktionerna såsom slide sidan.

---
+++ site/js/main.js
@@ -0,0 +1,22 @@
+function reveal(event){
+    let sak = document.querySelector(".top")
+    
+    let sak2 = document.querySelector(".blurr")
+    
+    
+    sak.classList.toggle("active")
+    sak2.classList.toggle("active")
+    
+    
+    
+    
+}
+
+function swits(event1){
+    let sak1 = document.querySelector(".box2")
+
+    let sak4 = document.querySelector(".box1")
+
+    sak1.classList.toggle("active1")
+    sak4.classList.toggle("active2")
+}
